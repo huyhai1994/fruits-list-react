aa484752e7f3c955ad158555914a81e834aa6090
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 05:07:22 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+/*TODO:    Sử dụng từ khoá import để thêm thư viện
+*/
+/*TODO: Buớc 3: Khai báo biến fruits mang giá trị là danh sách các loại quả muốn hiển thị*/
+const fruits = ["Apple", "Banana", "Orange", "Apricot", "Black rowan", "Cranberry"];
+/*TODO: Sử dụng const để khai báo biến fruits với giá trị ban đầu là [‘Apple’, ‘Banana’, ‘Orange’, ‘Apricot’, ‘Black rowan’, ‘Cranberry’]*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/*TODO: Khai báo biến root và khởi tạo với ReactDOM.createRoot(),
+    sau đó truyền element vừa tạo vào hàm root.render() để render element ra UI*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<div>
+    <h1>List of fruits</h1>
+    {/*TODO:    Sử dụng JSX để khởi tạo element mang giá trị hiển thị danh sách các loại quả
+*/}
+    <ul>
+        {fruits.map(item => (<li>{item}</li>))}
+    </ul>
+</div>);
